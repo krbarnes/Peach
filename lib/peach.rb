366304ff0957ff21bf6ad69018eb8ece2cd17e72
Kevin Barnes <kevin.barnes@shopify.com>
2019-04-03 15:36:21 -0400

    it works

--- lib/peach.rb
+++ lib/peach.rb
@@ -7,40 +7,18 @@ require 'peach/xc'
 
 class Peach
 
-	def self.delete_sim(device)
-		puts "Deleting #{device['name']}"
-		`xcrun simctl delete #{device['udid']}`
+	def self.destroy_all
+		Device.destroy_all(xc: XC.new)
 	end
 
-	def self.delete_all_sims
-		simctl_json = JSON.parse(`xcrun simctl list devices -j`)
-		device_list = simctl_json['devices']
-		device_list.each do |type, devices|
-			devices.each { |device| delete_sim(device) }
+	def self.createDevices(peachFile = 'peach.yml')
+		peaches = YAML.load_file(peachFile)
+		peaches.each do |properties|
+			peach_name = properties["name"]
+			peach_device = properties["device_type"]
+			peach_runtime = properties["runtime"]
+			peach = Device.new(name: peach_name, device_type: peach_device, runtime: peach_runtime)
+			peach.create() unless peach.exists?
 		end
 	end
-
-	def self.available_runtimes
-		runtimes_json = JSON.parse(`xcrun simctl list runtimes -j`)
-		runtimes = runtimes_json['runtimes']
-		runtime_hash = {}
-		runtimes.each do |runtime|
-			runtime_hash[runtime["name"]] = runtime["identifier"]
-		end
-		runtime_hash
-	end
-
-	def self.make_new_sims
-		runtimes = available_runtimes()
-
-		device_yml = YAML.load_file('peach.yml')
-		puts device_yml
-		device_yml.each do |properties|
-			runtime = runtimes[properties["runtime"]]
-			if runtime
-				`xcrun simctl create \"#{properties["name"]}\" \"#{properties["device_type"]}\" \"#{runtime}\"`
-			end
-		end
-	end
-
 end
